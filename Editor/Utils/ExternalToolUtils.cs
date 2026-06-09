38662522d9d218ef91f53c11fbc3bba509ab4c5f
gfool6 <gfool6@gmail.com>
2026-06-09 21:22:03 +0900

    update 1.0.1

--- Editor/Utils/ExternalToolUtils.cs
+++ Editor/Utils/ExternalToolUtils.cs
@@ -9,18 +9,76 @@ using UnityEditor;
 using UnityEditor.PackageManager;
 using UnityEditorInternal;
 using System.Threading.Tasks;
-using nadena.dev.modular_avatar.core;
 
 namespace FZTools
 {
+
+    [UnityEditor.InitializeOnLoad]
+    public class PackageInstallChecker
+    {
+        static PackageInstallChecker()
+        {
+            ExternalToolUtils.CheckInstalled();
+        }
+    }
+
     public static class ExternalToolUtils
     {
-        public static bool IsInstalledMA()
+        public static readonly string PACKAGE_MA = "nadena.dev.modular-avatar";
+        public static readonly string PACKAGE_AAO = "com.anatawa12.avatar-optimizer";
+        public static readonly string PACKAGE_LILYCAL = "jp.lilxyzw.lilycalinventory";
+
+        private static List<string> packageList = new() { PACKAGE_MA, PACKAGE_AAO, PACKAGE_LILYCAL };
+
+        private static Dictionary<string, bool> packageInstalledDict = null;
+
+        public static void CheckInstalled()
         {
-            var packageName = "nadena.dev.modular-avatar";
+            UnityEngine.Debug.LogWarning("CheckInstalled()");
+            if (packageInstalledDict != null)
+            {
+                UnityEngine.Debug.LogWarning("IS CHECKED!");
+                return;
+            }
+
             var packages = Client.List();
             while (!packages.IsCompleted) { }
-            return packages.Result.FirstOrDefault(p => p.name == packageName) != null;
+            packageInstalledDict = new()
+            {
+                {PACKAGE_MA, packages.Result.FirstOrDefault(p => p.name == PACKAGE_MA) != null},
+                {PACKAGE_AAO, packages.Result.FirstOrDefault(p => p.name == PACKAGE_AAO) != null},
+                {PACKAGE_LILYCAL, packages.Result.FirstOrDefault(p => p.name == PACKAGE_LILYCAL) != null},
+            };
+        }
+
+        public static bool IsInstalledMA()
+        {
+            if (packageInstalledDict != null)
+            {
+                return packageInstalledDict[PACKAGE_MA];
+            }
+            CheckInstalled();
+            return packageInstalledDict[PACKAGE_MA];
+        }
+
+        public static bool IsInstalledAAO()
+        {
+            if (packageInstalledDict != null)
+            {
+                return packageInstalledDict[PACKAGE_AAO];
+            }
+            CheckInstalled();
+            return packageInstalledDict[PACKAGE_AAO];
+        }
+
+        public static bool IsInstalledLilycal()
+        {
+            if (packageInstalledDict != null)
+            {
+                return packageInstalledDict[PACKAGE_LILYCAL];
+            }
+            CheckInstalled();
+            return packageInstalledDict[PACKAGE_LILYCAL];
         }
     }
 }
